6dab5255a5decb110ae4338a3adb8312a2c6b1ac
Patrick Odeh <vyltyan@gmail.com>
2024-11-30 16:26:20 +0100

    flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ def index():
     """
     Route for the index page.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
