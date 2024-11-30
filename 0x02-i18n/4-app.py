16dd2c62a7a1e90cb383b3cbb7e2e589ad8da7dc
Patrick Odeh <vyltyan@gmail.com>
2024-11-30 17:58:19 +0100

    flask

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """
 Flask app with Babel and translations
-Holberton
+This Flask application demonstrates
+internationalization (i18n) using Flask-Babel.
+It supports two languages: English and French. The app serves a homepage with
+translations based on the user's language preference.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
@@ -11,8 +14,12 @@ app = Flask(__name__)
 
 class Config:
     """
-    Configuration for Babel
-    Flask
+    Configuration class for the Flask app and Flask-Babel.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages for translation.
+        BABEL_DEFAULT_LOCALE (str): The default locale for the app (English).
+        BABEL_DEFAULT_TIMEZONE (str): The default timezone for the app (UTC).
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -37,7 +44,13 @@ def get_locale():
 
 @app.route('/')
 def index():
-    """Render the index template with translations."""
+    """Render the index template with translations.
+    This route renders the homepage with a title and header
+    that are translated based
+    on the current locale. The title and header texts
+    are retrieved using the `_()`
+    function for translation.
+    """
     return render_template('4-index.html',
                            title=_("home_title"),
                            header=_("home_header"))
