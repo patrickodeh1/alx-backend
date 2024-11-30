f410b3f22c3a66e7a935a45b440396e1bf4b7e60
Patrick Odeh <vyltyan@gmail.com>
2024-11-30 17:22:45 +0100

    flask

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,14 +4,16 @@ Flask app with Babel and translations
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
-
 app = Flask(__name__)
 
 
 class Config:
-    """Configuration for Babel"""
+    """
+    Configuration for Babel
+    Flask
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = "fr"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app.config.from_object(Config)
@@ -29,8 +31,8 @@ def get_locale():
 @app.route('/')
 def index():
     """Render the index template with translations."""
-    return render_template('3-index.html', 
-                           title=_("home_title"), 
+    return render_template('3-index.html',
+                           title=_("home_title"),
                            header=_("home_header"))
 
 
