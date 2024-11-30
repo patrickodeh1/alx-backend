00404c68789b2f2296509d8c7d495fe3dd98acb4
Patrick Odeh <vyltyan@gmail.com>
2024-11-30 16:23:41 +0100

    flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Basic Flask app with a single route.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Route for the index page.
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Flask app with Babel setup for i18n.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """
+    Configuration for Babel and Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    Route for the index page.
+    """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
